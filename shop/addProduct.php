071625706801a681abf65643751dc0276bfe5912
iAyzzz <112872245+iAyzzz@users.noreply.github.com>
2023-03-02 08:00:21 +0100

    Product

--- shop/addProduct.php
+++ shop/addProduct.php
@@ -2,15 +2,26 @@
 
     include "../header.php";
 
-    $product = \Stripe\Product::create();
-
     // var_dump($products);
 
+    if (isset($_POST['create'])) {
 
-    
-    foreach ($products->data as $products) {
-        echo 'Product: '. $products->name;
-        echo '<br> Beschrijving: '. $products->description;
+    } else {
+        ?>
+        <div class="max-w-md flex justify-center items-center">
+            <form action="" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <label for="naam" class="block text-gray-700 text-sm font-bold">Naam product: </label><br>
+                    <input type="text" id="naam" name="naam" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-4"><br>
+                    <label for="beschrijving" class="block text-gray-700 text-sm font-bold">Beschrijving: </label><br>
+                    <input type="text" name="beschrijving" id="beschrijving" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-4"><br>
+                    <label for="prijs" class="block text-gray-700 text-sm font-bold">Prijs: </label><br>
+                    <input type="text" name="prijs" id="prijs" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-6"><br>
+                    <label for="afbeelding" class="block text-gray-700 text-sm font-bold">Afbeelding: </label><br>
+                    <input type="file" name="afbeelding" id="afbeelding" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-6" accept="image/*"><br>
+                <button type="submit" name="create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Product toevoegen</button>
+            </form>
+        </div>
+        <?php
     }
     
 ?>
--- shop/shop.php
+++ shop/shop.php
@@ -4,18 +4,11 @@
 
     $products = \Stripe\Product::all();
 
-    // var_dump($products);
-
-
-
     foreach ($products->data as $product) {
         echo 'Product: '. $product->name;
         echo '<br> Beschrijving: '. $product->description;
-        $prijs = $stripe->prices->retrieve(
-          $product->default_price,
-          []
-        );
-        echo '<br> Prijs: '. $prijs->unit_amount;
+        $price = \Stripe\Price::retrieve($product->default_price);
+        echo '<br> Prijs: €'. $price->unit_amount / 100;
         echo '<br>';
     }
 
